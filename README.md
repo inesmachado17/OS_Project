69f98202c4e62d3ad85c9206b08069af2722f80d
mourabraz <cmourabraz@gmail.com>
2021-04-05 17:10:22 +0100

    refactor: add functions

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# OS_Project
-# 2200723 Ines Alexandra Ribeiro Machado
-# 2203845 Carlos Filipe de Moura Braz
+# OS - Word Stats Project
+
+ 2200723 Ines Alexandra Ribeiro Machado
+ 2203845 Carlos Filipe de Moura Braz
--- wordStats.sh
+++ wordStats.sh
@@ -1,88 +1,114 @@
 #! /usr/bin/env bash
 
-# Requirement check
-# Command pdftotext availability
-if [[ -z "$(which pdftotext)" ]]; then
-    echo "[ERROR] pdftotext: command not found"
-    exit 1
-fi
+#
+# Functions
+#
 
+# message display
+function display_error_msg_and_exit() {
+    echo -e "[ERROR] $1"
+    exit 1
+}
+function display_info_msg() {
+    echo -e "[INFO] $1"
+}
+function display_msg() {
+    echo -e "$1"
+}
+
+# requirement check
+function check_word_stats_top() {
+    if [ -z $WORD_STATS_TOP ]; then
+        display_msg "Environment variable 'WORD_STATS_TOP' is empty (using default 10)"
+        WORD_STATS_TOP=10
+    elif [[ $WORD_STATS_TOP =~ ^[1-9]+[0-9]*$ ]]; then
+        display_msg "WORD_STATS_TOP=$WORD_STATS_TOP"
+    else
+        display_msg "WORD_STATS_TOP: '$WORD_STATS_TOP' not a number (using default 10)"
+        WORD_STATS_TOP=10
+    fi
+}
+function check_language() {
+
+    lang_opt=$(echo $1 | tr "A-Z" "a-z")
+
+    if [[ ! "$lang_opt" =~ ^(pt|en)$ ]]; then
+        display_error_msg_and_exit "'$lang_opt' language not recognized\nChoose 'en' or 'pt'"
+    fi
+}
+function check_stop_words_file() {
+    if [[ $lang_opt != 'en' ]]; then
+        lang_file=$lang_opt".stop_words.txt"
+
+        if [ ! -f $lang_file ]; then
+            display_error_msg_and_exit "Can not find related stop words file for selected language"
+        fi
+    fi
+}
+
+# Global variables
 lang_file="en.stop_words.txt"
-lang_opt="en" #not mix with the global lang
+lang_opt="en" # not override the global lang variable
 mode=$1
 file=$2
 content=""
 
+# Command pdftotext availability
+if [[ -z "$(which pdftotext)" ]]; then
+    display_error_msg_and_exit "pdftotext: command not found"
+fi
+
+# Parameter check
 if [ $# -ne 2 ] && [ $# -ne 3 ]; then
-    echo "[ERROR] insufficient parameters"
-    echo "./word_stats.sh Cc|Pp|Tt INPUT [iso3166]"
-    exit 1
+    displayErrorMsg "insufficient parameters\n./word_stats.sh Cc|Pp|Tt INPUT [iso3166]"
 elif [[ ! "$mode" =~ ^(c|C|p|P|t|T)$ ]]; then
-    echo "[ERROR] unknown command '$mode'"
-    exit 1
+    display_error_msg_and_exit "unknown command '$mode'"
 elif [[ ! -f "$file" ]]; then
-    echo "[ERROR] can't find file '$file'"
-    exit 1
+    display_error_msg_and_exit "can't find file '$file'"
 fi
 
-# Optional parameter (language) check
-# pt or en (only)
-if [[ $# -eq 3 ]]; then
-    lang_opt=$(echo $3 | tr "A-Z" "a-z")
-fi
-if [[ ! "$lang_opt" =~ ^(pt|en)$ ]]; then
-    echo "[ERROR] '$lang_opt' language not recognized"
-    echo "Choose 'en' or 'pt'"
-    exit 1
-fi
-
-# Check if stop words file exists for chosen language
-if [[ "$mode" =~ [cpt] && $lang_opt != 'en' ]]; then
-    lang_file=$lang_opt".stop_words.txt"
-    if [ ! -f $lang_file ]; then
-        echo "[ERROR] Can not find related stop words file for selected language"
-        exit 1
-    fi
-fi
-
-# Check file type
-# pdf to text conversion
+# Check file type and content read
 if [[ $(file "$file" | cut -d':' -f 2 | grep -i text) ]]; then
     content=$(cat $file)
-    echo "'$file': TEXT file"
+    display_msg "'$file': TEXT file"
 elif [[ $(file "$file" | cut -d':' -f 2 | grep -i pdf) ]]; then
     content=$(pdftotext -nopgbrk $file -)
-    echo "'$file': PDF file"
+    display_msg "'$file': PDF file"
 else
-    echo "[ERROR] '$file' file type not supported"
-    exit 1
+    display_error_msg_and_exit "'$file' file type not supported"
 fi
-echo "[INFO] Processing '$file'"
+
+display_info_msg "Processing '$file'"
 
 # Remove numbers, punctuation, symbols and empty lines
-# https://web.fe.up.pt/~ee96100/projecto/Tabela%20ascii.htm
-# iconv //IGNORE garante que o file mantém UTF-8 depois do tr
-content=$(echo "$content" | tr "[:digit:]" " ")
-content=$(echo "$content" | tr "[:punct:]" " ")
-content=$(echo "$content" | tr -c "[A-Za-zÇ-Üá-ÑÁ-Àã-ÃÊ-ÏÓ-Ý]" " ")
-content=$(echo "$content" | tr "[:upper:]" "[:lower:]")
-content=$(echo "$content" | iconv -c -t UTF-8//IGNORE) #UTF-8 standardize
-content=$(echo "$content" | tr -s ' ' '\n')
+# iconv //IGNORE ensures the file is UTF-8 encoded after tr command
+content=$(echo "$content" |
+    tr "[:digit:]" " " |
+    tr "[:punct:]" " " |
+    tr -c "[A-Za-zÇ-Üá-ÑÁ-Àã-ÃÊ-ÏÓ-Ý]" " " |
+    tr "[:upper:]" "[:lower:]" |
+    iconv -c -t UTF-8//IGNORE |
+    tr -s ' ' '\n')
 
-# Remove stop words
+# Remove stop words mode
 if [[ "$mode" =~ [cpt] ]]; then
-    stop_words_content=$(cat "$lang_file" | xargs -0 -n1)
-    stop_words_content=$(echo "$stop_words_content" | tr -s '\n' '|')
+
+    check_language "$3"
+
+    check_stop_words_file
+
+    stop_words_content=$(cat "$lang_file" | xargs -0 -n1 | tr -s '\n' '|')
     stop_words_content=$(echo "${stop_words_content::-1}")
 
     content=$(echo "$content" | grep -Evi "^($stop_words_content)$")
-    echo "StopWords file '$lang_opt': 'StopWords/$lang_file' ($(wc -w $lang_file | cut -d' ' -f1) words)"
-    echo "STOP WORDS will be filtered out"
+
+    display_msg "StopWords file '$lang_opt': 'StopWords/$lang_file' ($(wc -w $lang_file | cut -d' ' -f1) words)"
+    display_msg "STOP WORDS will be filtered out"
 else
-    echo "STOP WORDS will be counted"
+    display_msg "STOP WORDS will be counted"
 fi
 
-# Sort and count ocurrencies on content
+# Sort and count ocurrences on content
 content=$(echo "$content" | sort | uniq -c | sort -nr)
 
 #
@@ -95,43 +121,43 @@ if [[ "$mode" =~ [c|C] ]]; then
     # save content output on a file
     echo "$content" >$result
 
-    echo "COUNT MODE"
+    display_msg "COUNT MODE"
     head $result
 
     lines=$(wc -l $result | cut -d' ' -f1)
 
     if [[ $lines -gt 10 ]]; then
-        echo "(...)"
+        display_msg "(...)"
     else
-        echo ""
+        display_msg ""
     fi
 
-    echo "RESULTS: '$result'"
-    echo $(ls -l $result)
+    display_msg "RESULTS: '$result'"
+    display_msg "$(ls -l $result)"
 
-    echo $(wc -w $result | cut -d' ' -f1) "distinct words"
+    display_msg "$(wc -w $result | cut -d' ' -f1) distinct words"
 fi
 
 if [[ "$mode" =~ [p|P] ]]; then
-    if [ -z $word_stats_top ]; then
-       word_stats_top=10        
-    # !! https://stackoverflow.com/questions/806906/how-do-i-test-if-a-variable-is-a-number-in-bash
-    elif [[ $word_stats_top =~ ^[1-9]+[0]*$ ]]; then
-        word_stats_top=$word_stats_top
-    fi
+
+    check_word_stats_top
+
     #https://stackoverflow.com/questions/22869025/gnuplot-change-value-of-x-axis
     result="results---"${file::-4}
-    content=$(echo "$content" | head -n $word_stats_top)
-    echo "$content">$result".dat"
-    date=$(TZ=Europe/Lisbon date +'%Y.%m.%d-%Hh%M:%S')
-        
+    content=$(echo "$content" | head -n $WORD_STATS_TOP)
+
+    # save content output on a file
+    echo "$content" >$result".dat"
+
+    today=$(TZ=Europe/Lisbon date +'%Y.%m.%d-%Hh%M:%S')
+
     if [[ $mode == 'p' ]]; then
-        title="Top words for '$file'\nCreated: $date\n('$lang_opt' stop words removed)"
+        title="Top words for '$file'\nCreated: $today\n('$lang_opt' stop words removed)"
     else
-        title="Top words for '$file'\nCreated: $date\n('$lang_opt' stop words counted)"
+        title="Top words for '$file'\nCreated: $today\n('$lang_opt' stop words counted)"
     fi
 
-    gnuplot << EOF
+    gnuplot <<EOF
         set xlabel "words"
         set ylabel "number of occurences"
         set title "${title}"
@@ -147,35 +173,41 @@ if [[ "$mode" =~ [p|P] ]]; then
             ''          using 0:1:1 with labels center boxed notitle
 EOF
 
-    html="<!DOCTYPE html><html lang=\"en\"><head> <meta charset=\"UTF-8\"> <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"> <title>Top Words</title></head><body> <h1>Top $word_stats_top Words - '${file}' </h1> <img src=\"$result.png\" alt=\"Top $word_stats_top words chart\"><p>Authors: Estudante 2200723, Estudante 2203845<br>Created: $date</p></body></html>"
-    echo $html>$result".html"
-    echo $(ls -l $result".dat")
-    echo $(ls -l $result".png")
-    echo $(ls -l $result".html")
+    cat >$result".html" <<EOF
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Top Words</title>
+        </head>
+        <body>
+            <h1>Top ${WORD_STATS_TOP} Words - '${file}'</h1>
+            <img src="${result}.png" alt="Top ${WORD_STATS_TOP} words chart">
+            <p>Authors: Estudante 2200723, Estudante 2203845<br>Created: ${today}</p>
+        </body>
+EOF
+
+    display_msg "$(ls -l $result".dat")"
+    display_msg "$(ls -l $result".png")"
+    display_msg "$(ls -l $result".html")"
+
     display $result".png"
 fi
 
 if [[ "$mode" =~ [t|T] ]]; then
 
-    if [ -z $word_stats_top ]; then
-        echo "Environment variable 'word_stats_top' is empty (using default 10)"
-        word_stats_top=10
-    # !! https://stackoverflow.com/questions/806906/how-do-i-test-if-a-variable-is-a-number-in-bash
-    elif [[ $word_stats_top =~ ^[1-9]+[0]*$ ]]; then
-        echo "word_stats_top=$word_stats_top"
-    else
-        echo "'$word_stats_top' not a number (using default 10)"
-        word_stats_top=10
-    fi
+    check_word_stats_top
 
-    content=$(echo "$content" | head -n $word_stats_top)
+    content=$(echo "$content" | head -n $WORD_STATS_TOP)
 
     result="results---"${file::-3}"txt"
     # save content output on a file
     echo "$content" >$result
-    echo $(ls -l $result)~12
-    echo "-------------------------------------"
-    echo "# TOP $word_stats_top elements"
+    display_msg "$(ls -l $result)"
+    display_msg "-------------------------------------"
+    display_msg "# TOP $WORD_STATS_TOP elements"
     cat -n $result
-    echo "-------------------------------------"
+    display_msg "-------------------------------------"
 fi
